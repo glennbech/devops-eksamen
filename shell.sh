10bfb7cb4273b78958d05009517c8e3320cced38
OpKris <kristofferopdahl@mail.com>
2020-11-16 13:18:47 +0100

    fixed shell.sh

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag devops-eksamen --build-arg JAR_FILE=./target/devOps-eksamen-1.0-SNAPSHOT.jar
+docker build . --tag devops-eksamen --build-arg JAR_FILE=./target/devops-eksamen-pgr301-1.0-SNAPSHOT.jar
 docker tag devops-eksamen opkris/devops-eksamen
 docker push opkris/devops-eksamen
