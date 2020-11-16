999221d3557f928f003df1b0925f07fdda4cafa4
OpKris <kristofferopdahl@mail.com>
2020-11-16 12:50:12 +0100

    added a simple test

---
+++ src/test/java/no/eksamen/devops/demoControllerTest.java
@@ -0,0 +1,24 @@
+package no.eksamen.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class demoControllerTest {
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void simpleTest() {
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+
+        assertEquals(15, c);
+
+    }
+
+}
