947d1e6fa0b487e7228299a7f3fd2e9d4d047c46
OpKris <kristofferopdahl@mail.com>
2020-11-20 13:07:30 +0100

    application is open

--- src/main/kotlin/no/eksamen/devops/RestAPI.kt
+++ src/main/kotlin/no/eksamen/devops/RestAPI.kt
@@ -19,26 +19,26 @@ class RestAPI(
         private val userService: UserService
 ) {
 
-//    @ApiOperation("Retrieve card collection information for a specific user")
-//    @GetMapping(path = ["/{userId}"])
-//    fun getUserInfo(
-//            @PathVariable("userId") userId: String
-//    ) : ResponseEntity<UserDto>{
-//
-//        val user = userService.findByIdEager(userId)
-//        if(user == null){
-//            return ResponseEntity.notFound().build()
-//        }
-//        return ResponseEntity.status(200).body(DtoConverter.transform(user))
-//    }
-//
-//    @ApiOperation("Create a new user, with the given id")
-//    @PutMapping(path = ["/{userId}"])
-//    fun createUser(
-//            @PathVariable("userId") userId: String
-//    ): ResponseEntity<Void>{
-//        val ok = userService.registerNewUser(userId)
-//        return if(!ok)  ResponseEntity.status(400).build()
-//            else ResponseEntity.status(201).build()
-//    }
+    @ApiOperation("Retrieve card collection information for a specific user")
+    @GetMapping(path = ["/{userId}"])
+    fun getUserInfo(
+            @PathVariable("userId") userId: String
+    ) : ResponseEntity<UserDto>{
+
+        val user = userService.findByIdEager(userId)
+        if(user == null){
+            return ResponseEntity.notFound().build()
+        }
+        return ResponseEntity.status(200).body(DtoConverter.transform(user))
+    }
+
+    @ApiOperation("Create a new user, with the given id")
+    @PutMapping(path = ["/{userId}"])
+    fun createUser(
+            @PathVariable("userId") userId: String
+    ): ResponseEntity<Void>{
+        val ok = userService.registerNewUser(userId)
+        return if(!ok)  ResponseEntity.status(400).build()
+            else ResponseEntity.status(201).build()
+    }
 }
