f8880b8bbf81931265092ba0662a26ff7e7f47aa
OpKris <kristofferopdahl@mail.com>
2020-11-21 11:00:02 +0100

    added logback.xml and some logger.info in RestAPI.kt

--- src/main/kotlin/no/eksamen/devops/RestAPI.kt
+++ src/main/kotlin/no/eksamen/devops/RestAPI.kt
@@ -13,6 +13,11 @@ import org.springframework.web.bind.annotation.*
 import no.eksamen.devops.dto.UserDto
 import no.eksamen.devops.DtoConverter
 import no.eksamen.devops.db.UserService
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+
+import java.util.List;
+import java.util.logging.Logger;
 
 @Api(value = "/api/user-collections", description = "Operations on card collections owned by users")
 @RequestMapping(
@@ -22,16 +27,20 @@ import no.eksamen.devops.db.UserService
 @RestController
 class RestAPI(
 
-
         private val userService: UserService
 ) {
+
+    @Autowired
+    private lateinit var registry: MeterRegistry
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
     @ApiOperation("Retrieve card collection information for a specific user")
     @GetMapping(path = ["/{userId}"])
     fun getUserInfo(
             @PathVariable("userId") userId: String
     ) : ResponseEntity<UserDto>{
-
+        logger?.info("Get user information. path = /userId")
         val user = userService.findByIdEager(userId)
         if(user == null){
             return ResponseEntity.notFound().build()
@@ -44,8 +53,9 @@ class RestAPI(
     fun createUser(
             @PathVariable("userId") userId: String
     ): ResponseEntity<Void>{
+        logger?.info("Create user. path = /userId")
         val ok = userService.registerNewUser(userId)
-        return if(!ok)  ResponseEntity.status(400).build()
+        return if(!ok) ResponseEntity.status(400).build()
             else ResponseEntity.status(201).build()
     }
 }
