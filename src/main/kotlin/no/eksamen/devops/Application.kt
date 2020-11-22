783eb71cde799cfa2128861c1a74b3f95c26e69c
OpKris <kristofferopdahl@mail.com>
2020-11-22 13:38:33 +0100

    new logz token/url encrypt

--- src/main/kotlin/no/eksamen/devops/Application.kt
+++ src/main/kotlin/no/eksamen/devops/Application.kt
@@ -12,22 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket
 
 @SpringBootApplication
 class Application{
-//    @Bean
-//    fun swaggerApi(): Docket {
-//        return Docket(DocumentationType.OAS_30)
-//                .apiInfo(apiInfo())
-//                .select()
-//                .paths(PathSelectors.any())
-//                .build()
-//    }
-//
-//    private fun apiInfo(): ApiInfo {
-//        return ApiInfoBuilder()
-//                .title("API for User-Collections")
-//                .description("REST service to handle the card collections owned by users")
-//                .version("1.0")
-//                .build()
-//    }
+
 }
 
 fun main(args: Array<String>) {
