f00300a1a10718e24d68fe8c7c4be8593e332494
OpKris <kristofferopdahl@mail.com>
2020-11-23 01:43:16 +0100

    created @Scheduling

--- src/main/kotlin/no/eksamen/devops/Application.kt
+++ src/main/kotlin/no/eksamen/devops/Application.kt
@@ -1,8 +1,15 @@
 package no.eksamen.devops
 
+import io.micrometer.core.annotation.Timed
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.ResponseEntity
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.service.ApiInfo
@@ -11,9 +18,8 @@ import springfox.documentation.spring.web.plugins.Docket
 
 
 @SpringBootApplication
-class Application{
-
-}
+@EnableScheduling
+class Application{}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
--- src/main/kotlin/no/eksamen/devops/RestAPI.kt
+++ src/main/kotlin/no/eksamen/devops/RestAPI.kt
@@ -2,23 +2,19 @@ package no.eksamen.devops
 
 import io.micrometer.core.annotation.Timed
 import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
-
-import io.micrometer.core.instrument.MeterRegistry
-import io.micrometer.core.instrument.Metrics
-import io.micrometer.core.instrument.Timer
-
-import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-
-import no.eksamen.devops.dto.UserDto
+import no.eksamen.devops.db.UserRepository
 import no.eksamen.devops.db.UserService
+import no.eksamen.devops.dto.UserDto
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import java.time.Duration
-import java.util.*
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.web.bind.annotation.*
 
 @Api(value = "/api/user-collections", description = "Operations on card collections owned by users")
 @RequestMapping(
@@ -28,7 +24,9 @@ import java.util.*
 @RestController
 class RestAPI(
 
-        private val userService: UserService
+        private val userService: UserService,
+
+        private  var userRepository: UserRepository
 
 
 ) {
@@ -41,7 +39,7 @@ class RestAPI(
 
     @ApiOperation("Retrieve card collection information for a specific user")
     @GetMapping(path = ["/{userId}"])
-    @Timed("Get User information", longTask = true)
+    @Timed("my.timer Get User information")
     fun getUserInfo(
             @PathVariable("userId") userId: String
     ): ResponseEntity<UserDto> {
@@ -54,22 +52,17 @@ class RestAPI(
             logger.warn("Error: tried to get $userId information ")
             return ResponseEntity.notFound().build()
         }
-        return ResponseEntity.status(200).body(DtoConverter.transform(user))
-    }
 
+        return ResponseEntity.status(200).body(DtoConverter.transform(user))
+
+    }
 
     @ApiOperation("Create a new user, with the given id")
     @PostMapping(path = ["/{userId}"])
-    @Timed("Time to create custom User")
     fun createUser(
             @PathVariable("userId") userId: String
     ): ResponseEntity<Void> {
 
-
-        if(userId == "dummy"){
-            logger.info("created dummyData")
-            userService.createDummyUsers()
-        }
         val ok = userService.registerNewUser(userId)
 
         return if (!ok) {
@@ -77,15 +70,45 @@ class RestAPI(
             logger.warn("error while created a new user")
             return ResponseEntity.status(400).build()
         } else {
-
-            Thread.sleep(5000)
+            if (userId == "dummy") {
+                // create a dummy base with simulation on metric counter
+                logger.info("created dummyData")
+                userService.createDummyUsers()
+            }
             logger.info("Successfully created regular user: $userId")
             ResponseEntity.status(201).build()
         }
     }
 
+
+    @Timed("my.timer longTask create user", longTask = true)
+    @Scheduled(fixedDelay = 60000L) // every 60 seconds
+    fun deleteEntireUserDataBase() {
+
+        //THIS IS NOT HOW TO DO IT !!!
+        val userId = "this_UserId_Is_To_Check_If_The_Data_Base_Is_Deleted_Or_Not_42_42_42"
+        userService.registerNewUser(userId)
+
+         userRepository.deleteAll()
+
+        // there are other ways and better to check of the userRepository is Deleted or not
+        // if the database was not deleted  the user will exist and cast a warning
+        return if (userRepository.existsById(userId)) {
+
+            logger.warn("error while created Scheduled_User")
+        } else {
+            logger.info("Successfully Deleted the database")
+        }
+    }
+
+
+
+
+
+
     @ApiOperation("Create a new custom user, with specific values")
     @PostMapping(path = ["/{userId}/{role}/{coins}/{cardpack}"])
+    @Timed("my.timer create custom User")
     fun customCustomUser(
             @PathVariable("userId") userId: String,
             @PathVariable("role") role: String,
@@ -97,7 +120,7 @@ class RestAPI(
 
         return if (!ok) {
 
-//            val counter = meterRegistry.counter("user coins","Coins",coins.toString()).increment()
+            //"faking error""
             logger.error("error while created a new custom user")
             return ResponseEntity.status(400).build()
         } else {
@@ -108,10 +131,8 @@ class RestAPI(
                 logger.info(" Admin has been created $userId")
             }
             val counter: Counter = meterRegistry.counter("my.counter")
-            val timer: Timer = meterRegistry.timer("my.timer")
 
             counter.increment(coins)
-            timer.record(Duration.ofMillis(500))
 
             logger.info("Successfully creating user: $userId,role: $role, $coins Coins, $cardPack card packs")
             ResponseEntity.status(201).build()
